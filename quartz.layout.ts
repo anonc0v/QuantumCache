7615064375e55d88c0cd31ac11f2a9ef9e1e85b4
pr1me <anonc0v@pr1me.local>
2024-10-18 09:45:35 -0600

    Quartz sync: Oct 18, 2024, 9:45 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,10 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [  Component.OnlyFor(
+    { titles: ["Eilleen's (online!) Everything Notebook"] },
+    Component.RecentNotes({ showTags: false, title: "Recently edited notes:", showDate: true })
+  ), ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
