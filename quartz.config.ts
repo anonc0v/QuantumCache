7d01ec31e55e0bf3dc5edf63e98f14e77b30e54a
pr1me <anonc0v@192.168.1.11>
2024-10-22 22:22:22 -0600

    Quartz sync: Oct 22, 2024, 10:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Test",
-    pageTitleSuffix: "Lomana",  
+    pageTitle: "(⌐■_■)",
+    pageTitleSuffix: "(⌐■_■)",  
     enableSPA: true,
     enablePopovers: true,
     analytics: {
