0a19bbd13866b6e4fa973efa37e3375ddf6a7304
pr1me <anonc0v@pr1me.local>
2024-10-27 19:03:44 -0600

    Quartz sync: Oct 27, 2024, 7:03 PM

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -46,9 +46,27 @@ export default ((userOpts?: Partial<Options>) => {
     const pages = allFiles.filter(opts.filter).sort(opts.sort)
     const remaining = Math.max(0, pages.length - opts.limit)
     return (
-      <div class={classNames(displayClass, "recent-notes")}>
-        <h3>{opts.title ?? i18n(cfg.locale).components.recentNotes.title}</h3>
-        <ul class="recent-ul">
+        <div class={classNames(displayClass, "recent-notes")}>
+          <h3>{opts.title ?? i18n(cfg.locale).components.recentNotes.title} 
+            <span class="see-more">
+              <span class="see-more">  </span>
+              <a href="https://github.com/fanteastick/quartz-test/commits/v4/content?author=fanteastick" class="external">see history</a>
+              <svg 
+                class="external-icon"
+                viewBox= "0 0 512 512"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                fill="currentColor"
+                xmlSpace="preserve"
+              >
+                <path 
+                  d= "M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"
+                />
+              </svg>
+            </span>
+
           {pages.slice(0, opts.limit).map((page) => {
             const title = page.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
             const tags = page.frontmatter?.tags ?? []
@@ -86,7 +104,7 @@ export default ((userOpts?: Partial<Options>) => {
               </li>
             )
           })}
-        </ul>
+        </h3>
         {opts.linkToMore && remaining > 0 && (
           <p>
             <a href={resolveRelative(fileData.slug!, opts.linkToMore)}>
