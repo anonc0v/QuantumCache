fe963c3c8ffd89952f37553ac6d488f33e472b61
pr1me <anonc0v@192.168.1.3>
2024-11-09 00:09:15 -0600

    Quartz sync: Nov 9, 2024, 12:09 AM

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -10,7 +10,7 @@ import { i18n } from "../i18n"
 
 // Options interface defined in `ExplorerNode` to avoid circular dependency
 const defaultOptions = {
-  folderClickBehavior: "collapse",
+  folderClickBehavior: "link", //changed 11/9 from "collapsed"
   folderDefaultState: "collapsed",
   useSavedState: true,
   mapFn: (node) => {
